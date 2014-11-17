2c10a4c887eab10ddb25b1dbb01c820012511c8a
13758116094 <wangxt@JSJ-1108-178.huawei>
2014-11-17 18:10:22 +0800

    add

--- shop/common/basicCss.php
+++ shop/common/basicCss.php
@@ -1,3 +1,5 @@
+
 <link type="text/css" rel="stylesheet"  href="assets/font-awesome/css/font-awesome.min.css" >
 <link type="text/css" rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" > 
+
 <link type="text/css" rel="stylesheet" href="assets/style.css" > 
